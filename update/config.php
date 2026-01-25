a49f9ab9a57fc9af3ecdedd8da41959892362cf4
Andreas Olsson <basen82@icloud.com>
2026-01-25 08:26:16 +0100

    Update to version 0.6.6

--- update/config.php
+++ update/config.php
@@ -27,5 +27,5 @@
  *                                               SOFTWARE.                                               *
  *********************************************************************************************************/
 $config['version_url'] = "https://update.rivendellwebsys.com";
-$config['version_file'] = "update-065.yml";
+$config['version_file'] = "update-066.yml";
 $config["update_folder"] = "../";
